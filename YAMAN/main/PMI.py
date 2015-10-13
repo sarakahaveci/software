be9b7442674ab4c795452be43e19434c4a5a9673
yamoonz <yamooonz@gmail.com>
2015-10-13 10:37:20 +0300

    Initial commit

---
+++ YAMAN/main/PMI.py
@@ -0,0 +1,4 @@
+__author__ = 'yamanalkahwaji'
+
+def size_of_the_list(A):
+    return len(A)
---
+++ YAMAN/test/TMI.py
@@ -0,0 +1,13 @@
+__author__ = 'yamanalkahwaji'
+
+import unittest
+
+import YAMAN.main.PMI
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(YAMAN.main.PMI.size_of_the_list([6,4,1]),3)
+
+
+if __name__ == '__main__':
+    unittest.main()
